9c0e4f433b5c062bfb2060378f639b450bcfce0c
Karen Pilosyan <karenpilosyan@Karens-Air.lan>
2024-03-13 21:46:06 +0400

    Add favicon.png; Change index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.module.scss';
 import App from './App';
 import authReducer from './state';
 import { Provider } from 'react-redux';
